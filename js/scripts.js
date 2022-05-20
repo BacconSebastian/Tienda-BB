017213a9ee79aea47f50204b70f259df850fde89
Sebastian <bacconsebastian@gmail.com>
2022-05-19 23:36:53 -0300

    Pagina principal semi terminada

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,13 @@ window.addEventListener('scroll', () =>{
     nav.classList.toggle('active', window.scrollY > 65)
     corrector.classList.toggle('active', window.scrollY > 65)
 })
+
+const imagen_uno = document.querySelector('.imagen_uno');
+const imagen_dos = document.querySelector('.imagen_dos');
+
+
+window.setInterval(() => {
+    imagen_uno.classList.toggle('active')
+    imagen_dos.classList.toggle('active')
+    setTimeout
+}, 5000);
